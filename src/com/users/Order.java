051c741606eb1b8019f005c3be127e2d1081e323
Drago404 <dragovd4@gmail.com>
2018-04-20 18:12:12 +0300

    update

--- src/com/users/Order.java
+++ src/com/users/Order.java
@@ -1,7 +1,49 @@
 package com.users;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Order {
 
+	private int id;
+	private Map items;
+	private String ordererFirstName;
+	private String ordererLastName;
+	private String ordererCity;
+	private String ordererPhone;
+	private String ordererPostCode;
+	private String ordererStreet;
+	private int ordererStreetNumber;
+	private String ordererBlock;
+	private String ordererEnterance;
+	private int ordererFloor;
+	private String ordererAppartment;
+	private String description;
+	private int userId;
+
+	public Order(int id, String ordererFirstName, String ordererLastName, String ordererCity, String ordererPhone,
+			String ordererPostCode, String ordererStreet, int userId) {
+		this.id = id;
+		this.ordererFirstName = ordererFirstName;
+		this.ordererLastName = ordererLastName;
+		this.ordererCity = ordererCity;
+		this.ordererPhone = ordererPhone;
+		this.ordererPostCode = ordererPostCode;
+		this.ordererStreet = ordererStreet;
+		this.userId = userId;
+		this.items = new HashMap<Integer, Item>();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Map getItems() {
+		return Collections.unmodifiableMap(items);
+	}
+
 	
-	private float price;
+
 }
--- src/com/users/User.java
+++ src/com/users/User.java
@@ -1,20 +1,88 @@
 package com.users;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 	
-	private String name;
+	private int id;
+	private String firstName;
+	private String secondName;
+	private String email;
+	private String password;
+	private boolean isMale;
+	private LocalDate dateOfBirth;
+	private List orders;
+	
+	
+	public User(int id, String firstName, String secondName, String email, String password, boolean isMale,
+			LocalDate dateOfBirth) {
+		this.id = id;
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.email = email;
+		this.password = password;
+		this.isMale = isMale;
+		this.dateOfBirth = dateOfBirth;
+		this.orders = new ArrayList<Order>();
+	}
+	
 
-	public User(String name) {
-		this.name = name;
+	public int getId() {
+		return id;
 	}
 
-	public String getName() {
-		return name;
+
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isMale() {
+		return isMale;
+	}
+
+	public void setMale(boolean isMale) {
+		this.isMale = isMale;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	
 	
 	
 	
